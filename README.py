d35b7945ccda54d80cca9bfbc3d0d26fe84aa5fd
Felipe Sant Ana <35671903+FelipeSantanaIV@users.noreply.github.com>
2022-03-19 01:16:01 -0300

    Python

--- README.py
+++ README.py
@@ -1,6 +1,8 @@
-- 👋 Olá, meu nome é Felipe Sant Ana
-- 👀 Eu estou interessado em aprender.
-- 🌱 Eu estou aprendendo Java.
+# - 👋 Olá, meu nome é Felipe Sant Ana
+# - 👀 Eu estou interessado em aprender.
+# - 🌱 Eu estou aprendendo.
+
+print('Ola mundo!)
 
 <!---
 virus2805/virus2805 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
