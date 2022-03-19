f92b397ed6c0a6dcad33034d02460fb8d471dae4
Felipe Sant Ana <35671903+FelipeSantanaIV@users.noreply.github.com>
2022-03-19 01:16:27 -0300

    Update README.py

--- README.py
+++ README.py
@@ -3,8 +3,3 @@
 # - 🌱 Eu estou aprendendo.
 
 print('Ola mundo!)
-
-<!---
-virus2805/virus2805 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
